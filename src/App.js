40c54f3c5e2c3368295ac636d26bf949a20a5e90
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-07-21 23:03:34 -0500

    componente Error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import Header from './Components/Header';
 import SearchForm from './Components/SearchForm'
+import Error from './Components/Error';
 
 function App() {
 
 	const [ciudad, guardarCiudad] = useState('')
 	const [pais, guardarPais] = useState('')
+	const [error, guardarError] = useState('')
 
 	const datosConsulta = datos => {
 		// validacion de datos
 		if (datos.ciudad === '' || datos.pais === '') {
-			
+			guardarError(true)
 			return
 		}
 
 		guardarCiudad(datos.ciudad)
 		guardarPais(datos.pais)
+		guardarError(false)
 	}
+
+	let ErrorComp = error ? <Error mensaje='Ambos campos son obligatorios' /> : null
 
 	return (
 		<div className="App">
@@ -28,6 +33,9 @@ function App() {
 						<div className="col s12 m6">
 							<SearchForm datosConsulta={datosConsulta} />
 						</div>
+						<div className="col s12 m6">
+							{ErrorComp}
+						</div>						
 					</div>
 				</div>
 			</div>
